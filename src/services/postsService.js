5f4a092d9fe70ec7162f4d4f921784046ea6dc29
andres <anrofe1321@gmail.com>
2025-02-21 13:06:14 -0800

    commit

--- src/services/postsService.js
+++ src/services/postsService.js
@@ -1,6 +1,6 @@
 
 
-const BASE_URL =  `${import.meta.env.VITE_BACK_END_SERVER_URL}/users`
+const BASE_URL =  `${import.meta.env.VITE_BACK_END_SERVER_URL}/posts`
 
 
 async function create(formData){
