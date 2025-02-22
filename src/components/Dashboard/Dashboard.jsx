d411d5d4eed3241a852c06d43b10edd6977c6a00
KrisO95 <kristianobrien95@gmail.com>
2025-02-21 19:30:26 -0500

    added code to render list

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,48 +1,38 @@
 // src/components/Dashboard/Dashboard.jsx
 
-import { useEffect, useContext } from 'react';
+import { useEffect, useContext } from "react";
 
-import { UserContext } from '../../contexts/UserContext';
+import { UserContext } from "../../contexts/UserContext";
 
 import { Link } from "react-router";
 
-import * as userService from '../../services/userService';
+import * as userService from "../../services/userService";
 
-import './Dashboard.css'; // Import the CSS file 
+import "./Dashboard.css"; // Import the CSS file
 
-
-const Dashboard = () => {
+const Dashboard = (props) => {
   const { user } = useContext(UserContext);
 
-  // useEffect(() => {
-
-  //   // THIS IS AN EXAMPLE OF AN API CALL 
-  //   // AFTER YOU ARE LOGGED IN, PLEASE LOOK AT THE USERSERVICE
-  //   // HEADERS FOR SENDING THE JWT TOKEN OVER
-
-
-  //   const fetchUsers = async () => {
-  //     try {
-  //       const fetchedUsers = await userService.index();
-  //       console.log(fetchedUsers);
-  //     } catch (err) {
-  //       console.log(err)
-  //     }
-  //   }
-  //   if (user) fetchUsers();
-  // }, [user.username]); // this useEffect is running when component loads, or when the value
-  // // of user changes
+  const carLis = props.cars.map((car) => {
+    return (
+      <li key={car._id} onClick={() => props.setCar(car)}>
+        {car.brand}: {car.model}: {car.year}
+      </li>
+    );
+  });
 
   return (
     <main>
       <h1>Welcome, {user.username}</h1>
+      {carLis}
       <p>
         This is the dashboard page where you can see a list of all the cars.
-        <li><Link to='/cars/new'>Create Car</Link></li>
+        <li>
+          <Link to="/cars/new">Create Car</Link>
+        </li>
       </p>
     </main>
   );
 };
 
 export default Dashboard;
-
