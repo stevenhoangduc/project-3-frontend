c65642ce27f93a32c080f2bcd409297ec2b2565e
KrisO95 <kristianobrien95@gmail.com>
2025-02-21 19:04:41 -0500

    fixed frontend

--- src/components/CarList/CarList.jsx
+++ src/components/CarList/CarList.jsx
@@ -1,27 +1,51 @@
 import './Carlist.css'
 import { Link } from 'react-router'
 
-export default function CarList(props){
 
-    const carLis = props.cars.map((car) => {
-        return (
-        <li key={car._id} >
-            <Link to={`/cars/${car._id}`}>{car.brand}</Link>
+
+
+export default function CarList(props) {
+    const carLis = props.car.map((car) => {
+      //returning a list of tracks using the track iD and the .map method
+      return (
+        <li key={car._id} onClick={() => props.setCar(car)}>
+          {car.brand}: {car.model}: {car.year}
         </li>
-        )
-    })
-
+      );
+    });
+  
     return (
-        <section className={'car-list'}>
-            <h1>Car List</h1>
-            {carLis.length !== 0 ? (
-                <ul>
-                    {carLis}
-                    <CarList setSelectedCar={setSelectedCar} cars={cars} handleFormOpen={handleFormOpen} buttonTextForForm={buttonTextForForm} />
-                </ul>
-            ) : (
-                <h2>No Cars yet!</h2>
-            )}
-        </section>
-    )
-}
+      <section>
+        <h1>Car List</h1>
+        {carLis.length !== 0 ? ( //If the tracklist has something in the Array...
+          <ul>{carLis}</ul>
+        ) : (
+          <h2>No Cars yet!</h2> // ^ Log the tracks if there isn't any tracks to log, respond with message listed.
+        )}
+      </section>
+    );
+  }
+
+// export default function CarList(props){
+
+//     const carLis = props.cars.map((car) => {
+//         return (
+//         <li key={car._id} onClick={() => props.setSelectedCar(car)} >
+//             <Link to={`/cars/${car._id}`}>{car.brand}</Link>
+//         </li>
+//         )
+//     })
+
+//     return (
+//         <section className={'car-list'}>
+//             <h1>Car List</h1>
+//             {carLis.length !== 0 ? (
+//                 <ul>
+//                     {carLis}
+//                 </ul>
+//             ) : (
+//                 <h2>No Cars yet!</h2>
+//             )}
+//         </section>
+//     )
+// }
--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -14,24 +14,24 @@ import './Dashboard.css'; // Import the CSS file
 const Dashboard = () => {
   const { user } = useContext(UserContext);
 
-  useEffect(() => {
+  // useEffect(() => {
 
-    // THIS IS AN EXAMPLE OF AN API CALL 
-    // AFTER YOU ARE LOGGED IN, PLEASE LOOK AT THE USERSERVICE
-    // HEADERS FOR SENDING THE JWT TOKEN OVER
+  //   // THIS IS AN EXAMPLE OF AN API CALL 
+  //   // AFTER YOU ARE LOGGED IN, PLEASE LOOK AT THE USERSERVICE
+  //   // HEADERS FOR SENDING THE JWT TOKEN OVER
 
 
-    const fetchUsers = async () => {
-      try {
-        const fetchedUsers = await userService.index();
-        console.log(fetchedUsers);
-      } catch (err) {
-        console.log(err)
-      }
-    }
-    if (user) fetchUsers();
-  }, [user.username]); // this useEffect is running when component loads, or when the value
-  // of user changes
+  //   const fetchUsers = async () => {
+  //     try {
+  //       const fetchedUsers = await userService.index();
+  //       console.log(fetchedUsers);
+  //     } catch (err) {
+  //       console.log(err)
+  //     }
+  //   }
+  //   if (user) fetchUsers();
+  // }, [user.username]); // this useEffect is running when component loads, or when the value
+  // // of user changes
 
   return (
     <main>
